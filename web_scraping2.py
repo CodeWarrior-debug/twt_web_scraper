84e95b17e7e8d1f4c168f47d3826a9c22bba43b2
CodeWarrior-debug <jpjordan1987@gmail.com>
2022-05-14 00:21:15 -0400

    almost through video 2

--- web_scraping2.py
+++ web_scraping2.py
@@ -1,5 +1,10 @@
-import selenium
+# import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH ='/Users/jamesjordan/GitHub/twt_web_scraper/chromedriver'
 
@@ -7,6 +12,19 @@ driver = webdriver.Chrome(PATH)
 
 driver.get('https://techwithtim.net')
 print(driver.title)
+
+search = driver.find_element_by_name('s')
+search.send_keys('test')
+search.send_keys(Keys.RETURN)
+
+try: 
+    main = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "main"))
+    )
+    print(main.text)
+except: 
+    driver.quit()
+
+
 driver.quit()
 
 
